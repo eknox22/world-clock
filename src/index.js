ca23a969cfe9238cbd837ab4deed9c028b0e7d46
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-07 21:43:14 +0100

    Added parisElement to target this timezone

--- src/index.js
+++ src/index.js
@@ -7,7 +7,18 @@ function updateTime() {
   let losAngelesTime = moment().tz("America/Los_Angeles");
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
   );
 }
 
