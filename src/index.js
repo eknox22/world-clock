a7d10b6c15c7a52cdca7ce902ccb3004655b5aa4
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-07 21:12:35 +0100

    Adding moment.js to the time and date classes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML = "May 7th 2024";
-losAngelesTimeElement.innerHTML = "9:04 <small>PM</small>";
+
+let losAngelesTime = moment();
+losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = moment().format("HH:mm:ss");
