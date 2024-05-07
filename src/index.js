74073836b8a10258b6cb60a1ad06b985559656aa
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-07 21:06:37 +0100

    Add moment.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML = "May 7th 2024";
+losAngelesTimeElement.innerHTML = "9:04 <small>PM</small>";
