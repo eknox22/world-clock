6ac5e0c01002b25083359fe5974945103f142390
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-08 21:35:12 +0100

    Added in the functionality for the select
    Added an ID of city under select.
    Added timezones to the value, to get this from JavaScript.

--- src/index.js
+++ src/index.js
@@ -20,15 +20,15 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace('_","").replace');
-  if ((cityTimeZone = moment().tz(cityTimeZone)));
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city">
           <div>
             <h2>${cityName}</h2>
-            <div class="date">${cityTimeZone.format("dddd MMM Do YYYY")}</div>
+            <div class="date">${cityTime.format("dddd MMM Do YYYY")}</div>
           </div>
-          <div class="time">${cityTimeZone.format("HH:mm:ss")}</div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
         </div>`;
 }
 
